26cc560f4b8ff1b903909546683af7095c20ccc8
Alexander Ivanov <alexfinn@protonmail.ch>
2016-10-11 12:45:54 +0300

    Renamed main.go to gce-meta.go

--- gce-meta.go
+++ gce-meta.go
@@ -1,209 +1,19 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/alexfinn/gce-meta/cmd"
 	"gopkg.in/urfave/cli.v1"
-	"io/ioutil"
-	"net/http"
 	"os"
-	"strings"
-	"sync"
 )
-
-type NotDefinedError string
-
-func (suffix NotDefinedError) Error() string {
-	return fmt.Sprintf("metadata: GCE metadata %q not defined", string(suffix))
-}
-
-func Get(suffix string) (string, error) {
-	val, _, err := getETag(suffix)
-	return val, err
-}
-
-func getETag(suffix string) (value, etag string, err error) {
-	// Using a fixed IP makes it very difficult to spoof the metadata service in
-	// a container, which is an important use-case for local testing of cloud
-	// deployments. To enable spoofing of the metadata service, the environment
-	// variable GCE_METADATA_HOST is first inspected to decide where metadata
-	// requests shall go.
-	client := &http.Client{}
-	host := os.Getenv("GCE_METADATA_HOST")
-	if host == "" {
-		// Using 169.254.169.254 instead of "metadata" here because Go
-		// binaries built with the "netgo" tag and without cgo won't
-		// know the search suffix for "metadata" is
-		// ".google.internal", and this IP address is documented as
-		// being stable anyway.
-		host = "169.254.169.254"
-	}
-	url := "http://" + host + "/computeMetadata/v1/" + suffix
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Set("Metadata-Flavor", "Google")
-	res, err := client.Do(req)
-	if err != nil {
-		return "", "", err
-	}
-	defer res.Body.Close()
-	if res.StatusCode == http.StatusNotFound {
-		return "", "", NotDefinedError(suffix)
-	}
-	if res.StatusCode != 200 {
-		return "", "", fmt.Errorf("status code %d trying to fetch %s", res.StatusCode, url)
-	}
-	all, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return "", "", err
-	}
-	return string(all), res.Header.Get("Etag"), nil
-}
-
-func getTrimmed(suffix string) (s string, err error) {
-	s, err = Get(suffix)
-	s = strings.TrimSpace(s)
-	return
-}
-
-var onGCE struct {
-	sync.Mutex
-	set bool
-	v   bool
-}
-
-// OnGCE reports whether this process is running on Google Compute Engine.
-func OnGCE() bool {
-	defer onGCE.Unlock()
-	onGCE.Lock()
-	if onGCE.set {
-		return onGCE.v
-	}
-	onGCE.set = true
-
-	client := &http.Client{}
-
-	// We use the DNS name of the metadata service here instead of the IP address
-	// because we expect that to fail faster in the not-on-GCE case.
-	res, err := client.Get("http://metadata.google.internal")
-	if err != nil {
-		return false
-	}
-	onGCE.v = res.Header.Get("Metadata-Flavor") == "Google"
-	return onGCE.v
-}
-
-// ProjectID returns the current instance's project ID string.
-func ProjectID() (string, error) {
-	return getTrimmed("project/project-id")
-}
-
-// NumericProjectID returns the current instance's numeric project ID.
-func NumericProjectID() (string, error) {
-	return getTrimmed("project/numeric-project-id")
-}
-
-// InternalIP returns the instance's primary internal IP address.
-func InternalIP() (string, error) {
-	return getTrimmed("instance/network-interfaces/0/ip")
-}
-
-// ExternalIP returns the instance's primary external (public) IP address.
-func ExternalIP() (string, error) {
-	return getTrimmed("instance/network-interfaces/0/access-configs/0/external-ip")
-}
-
-// Hostname returns the instance's hostname. This will be of the form
-// "<instanceID>.c.<projID>.internal".
-func Hostname() (string, error) {
-	return getTrimmed("instance/hostname")
-}
-
-// MachineType returns the instance's machine type.
-func MachineType() (string, error) {
-	machine, err := getTrimmed("instance/machine-type")
-	// machine-type is of the form "projects/<projNum>/machineTypes/<machine-typeName>
-	if err != nil {
-		return "", err
-	}
-	return machine[strings.LastIndex(machine, "/")+1:], nil
-}
-
-// Description returns the instance's description.
-func Description() (string, error) {
-	return getTrimmed("instance/description")
-}
-
-// InstanceTags returns the list of user-defined instance tags,
-// assigned when initially creating a GCE instance.
-func InstanceTags() ([]string, error) {
-	var s []string
-	j, err := Get("instance/tags")
-	if err != nil {
-		return nil, err
-	}
-	if err := json.NewDecoder(strings.NewReader(j)).Decode(&s); err != nil {
-		return nil, err
-	}
-	return s, nil
-}
-
-// InstanceID returns the current VM's numeric instance ID.
-func InstanceID() (string, error) {
-	return getTrimmed("instance/id")
-}
-
-// InstanceName returns the current VM's instance ID string.
-func InstanceName() (string, error) {
-	host, err := Hostname()
-	if err != nil {
-		return "", err
-	}
-	return strings.Split(host, ".")[0], nil
-}
-
-// Zone returns the current VM's zone, such as "us-central1-b".
-func Zone() (string, error) {
-	zone, err := getTrimmed("instance/zone")
-	// zone is of the form "projects/<projNum>/zones/<zoneName>".
-	if err != nil {
-		return "", err
-	}
-	return zone[strings.LastIndex(zone, "/")+1:], nil
-}
-
-// InstanceAttributes returns the list of user-defined attributes,
-// assigned when initially creating a GCE VM instance. The value of an
-// attribute can be obtained with InstanceAttributeValue.
-func InstanceAttributes() ([]string, error) { return lines("instance/attributes/") }
-
-func lines(suffix string) ([]string, error) {
-	j, err := Get(suffix)
-	if err != nil {
-		return nil, err
-	}
-	s := strings.Split(strings.TrimSpace(j), "\n")
-	for i := range s {
-		s[i] = strings.TrimSpace(s[i])
-	}
-	return s, nil
-}
-
-// InstanceAttributeValue returns the value of the provided VM
-// instance attribute.
-//
-// If the requested attribute is not defined, the returned error will
-// be of type NotDefinedError.
-//
-// InstanceAttributeValue may return ("", nil) if the attribute was
-// defined to be the empty string.
-func InstanceAttributeValue(attr string) (string, error) {
-	return Get("instance/attributes/" + attr)
-}
 
 func main() {
 	app := cli.NewApp()
 
+	app.EnableBashCompletion = true
+	app.Name = "gce-meta"
 	app.Version = "0.2"
+	app.Usage = "Small and simple utility to get some metadata of project or instance on Google Cloud Engine."
 
 	if len(os.Args) < 2 || len(os.Args) > 2 {
 		app.Action = func(c *cli.Context) error {
@@ -214,10 +24,12 @@ func main() {
 
 	app.Commands = []cli.Command{
 		{
-			Name:  "project-id",
-			Usage: "The project ID.",
+			Name:     "project-id",
+			Aliases:  []string{"p"},
+			Usage:    "The project ID.",
+			Category: "project",
 			Action: func(c *cli.Context) error {
-				id, err := ProjectID()
+				id, err := cmd.ProjectID()
 				if err != nil {
 					return err
 				}
@@ -226,10 +38,12 @@ func main() {
 			},
 		},
 		{
-			Name:  "numeric-project-id",
-			Usage: "The numeric project ID of the instance, which is not the same as the project name visible in the Google Cloud Platform Console.",
+			Name:     "numeric-project-id",
+			Aliases:  []string{"np"},
+			Usage:    "The numeric project ID of the instance, which is not the same as the project name visible in the Google Cloud Platform Console.",
+			Category: "project",
 			Action: func(c *cli.Context) error {
-				id, err := NumericProjectID()
+				id, err := cmd.NumericProjectID()
 				if err != nil {
 					return err
 				}
@@ -238,10 +52,11 @@ func main() {
 			},
 		},
 		{
-			Name:  "desc",
-			Usage: "The free-text description of an instance, assigned using the --description flag, or set in the API.",
+			Name:     "desc",
+			Usage:    "The free-text description of an instance, assigned using the --description flag, or set in the API.",
+			Category: "instance",
 			Action: func(c *cli.Context) error {
-				desc, err := Description()
+				desc, err := cmd.Description()
 				if err != nil {
 					return err
 				}
@@ -250,10 +65,11 @@ func main() {
 			},
 		},
 		{
-			Name:  "hostname",
-			Usage: "The full host name of the instance.",
+			Name:     "hostname",
+			Usage:    "The full host name of the instance.",
+			Category: "instance",
 			Action: func(c *cli.Context) error {
-				hostname, err := Hostname()
+				hostname, err := cmd.Hostname()
 				if err != nil {
 					return err
 				}
@@ -262,10 +78,12 @@ func main() {
 			},
 		},
 		{
-			Name:  "instance-name",
-			Usage: "The short host name of the instance.",
+			Name:     "instance-name",
+			Aliases:  []string{"name"},
+			Usage:    "The short host name of the instance.",
+			Category: "instance",
 			Action: func(c *cli.Context) error {
-				name, err := InstanceName()
+				name, err := cmd.InstanceName()
 				if err != nil {
 					return err
 				}
@@ -274,10 +92,12 @@ func main() {
 			},
 		},
 		{
-			Name:  "instance-id",
-			Usage: "The ID of the instance. This is a unique, numerical ID that is generated by Google Compute Engine.",
+			Name:     "instance-id",
+			Aliases:  []string{"id"},
+			Usage:    "The ID of the instance. This is a unique, numerical ID that is generated by Google Compute Engine.",
+			Category: "instance",
 			Action: func(c *cli.Context) error {
-				id, err := InstanceID()
+				id, err := cmd.InstanceID()
 				if err != nil {
 					return err
 				}
@@ -286,10 +106,12 @@ func main() {
 			},
 		},
 		{
-			Name:  "machine-type",
-			Usage: "The fully-qualified machine type name of the instance's host machine.",
+			Name:     "machine-type",
+			Aliases:  []string{"mt"},
+			Usage:    "The fully-qualified machine type name of the instance's host machine.",
+			Category: "instance",
 			Action: func(c *cli.Context) error {
-				machine, err := MachineType()
+				machine, err := cmd.MachineType()
 				if err != nil {
 					return err
 				}
@@ -298,14 +120,85 @@ func main() {
 			},
 		},
 		{
-			Name:  "zone",
-			Usage: "The instance's zone.",
+			Name:     "zone",
+			Usage:    "The instance's zone.",
+			Category: "instance",
 			Action: func(c *cli.Context) error {
-				zone, err := Zone()
+				zone, err := cmd.Zone()
 				if err != nil {
 					return err
 				}
 				fmt.Println(zone)
+				return nil
+			},
+		},
+		{
+			Name:     "internal-ip",
+			Aliases:  []string{"iip"},
+			Usage:    "Internal IP of instance.",
+			Category: "instance",
+			Action: func(c *cli.Context) error {
+				ip, err := cmd.InternalIP()
+				if err != nil {
+					return err
+				}
+				fmt.Println(ip)
+				return nil
+			},
+		},
+		{
+			Name:     "external-ip",
+			Aliases:  []string{"eip"},
+			Usage:    "External IP of instance.",
+			Category: "instance",
+			Action: func(c *cli.Context) error {
+				ip, err := cmd.ExternalIP()
+				if err != nil {
+					return err
+				}
+				fmt.Println(ip)
+				return nil
+			},
+		},
+		{
+			Name:     "tags",
+			Aliases:  []string{"t"},
+			Usage:    "Any tags associated with the instance.",
+			Category: "instance",
+			Action: func(c *cli.Context) error {
+				tags, err := cmd.InstanceTags()
+				if err != nil {
+					return err
+				}
+				fmt.Println(tags)
+				return nil
+			},
+		},
+		{
+			Name:     "attr",
+			Aliases:  []string{"a"},
+			Usage:    "Attributes passed to the instance during startup or shutdown.",
+			Category: "instance",
+			Subcommands: []cli.Command{
+				{
+					Name:  "get",
+					Usage: "Get attribute's value.",
+					Action: func(c *cli.Context) error {
+						value, err := cmd.InstanceAttributeValue(c.Args().First())
+						if err != nil {
+							return err
+						}
+						fmt.Println(value)
+						return nil
+					},
+				},
+			},
+			Action: func(c *cli.Context) error {
+				attr, err := cmd.InstanceAttributes()
+				if err != nil {
+					return err
+				}
+				fmt.Println(attr)
 				return nil
 			},
 		},
